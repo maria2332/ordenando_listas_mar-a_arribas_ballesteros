51c2814d949bd7d58a6deb7c19b9db63a87437dc
maria2332 <113980788+maria2332@users.noreply.github.com>
2022-10-18 16:40:09 +0000

    ejercicio2

---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+def ejercicio2():      
+  lista = [1, 4, 2, 5, 4, 3, 4, 7, 5, 8, 9]  
+  del lista [1]
+  del lista [2]
+  del lista [2]
+  del lista [4]
+  del lista [5:]
+  #TODO
+  assert lista == list(range(1, 6))
